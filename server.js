8224d9579984e0b9cdad3cd1298b29b83adc5b94
Sanket Kenguva (Brillio LLC) <v-sakeng@microsoft.com>
2018-09-17 18:30:18 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log =`${now}: ${req.method} ${req.url}`;
+
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (error) => {
+        if(error) {
+            console.log('Unable to append server.log.');
+        }
+    });
+    next();
+});
+
+// app.use((req, res, next) => {
+//     res.render('maintainance.hbs');
+// });
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+});
+
+app.get('/', (req, res) => {
+    //res.send('<h1>Hello Express!</h1>');
+    // res.send({
+    //     name: 'sanket',
+    //     likes: [
+    //         'Biking',
+    //         'Cities'        
+    //     ]
+    // });
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        name: 'sanket',
+        likes: [
+            'Biking',
+            'Cities'        
+        ]
+    });
+});
+
+app.get('/about', (req, res) => {
+    //res.send('about');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({        
+        ErrorMessage: 'Unable to handle the request'
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is up on port 3000');
+});
